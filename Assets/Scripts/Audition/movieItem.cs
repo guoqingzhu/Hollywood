b34c3896c68bbe2ecf5451e9b02c4d9cf36514ab
guoqing.zhu <1521292690@qq.com>
2023-12-27 14:49:07 +0800

    update

--- Assets/Scripts/Audition/movieItem.cs
+++ Assets/Scripts/Audition/movieItem.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class movieItem : MonoBehaviour
 {
-    public void onClickChoose() { }
+    public GameObject filmPic;
+    public GameObject fileName;
+    public GameObject director;
+
+    public void OnClickChoose() { }
 }
