5ba5e98d07daa4f13f6f509a62ce6ba5cd0316ab
zhuguoqing <1521292690@qq.com>
2024-02-02 11:19:33 +0800

    update

--- Assets/Scripts/MainScene.cs
+++ Assets/Scripts/MainScene.cs
@@ -14,6 +14,9 @@ public class MainScene : MonoBehaviour
 
     private void Start()
     {
+
+        //PlayerPrefs.DeleteAll();
+
         //用于确保已经登记过信息
         var hasRegist = PlayerPrefs.GetInt("hasRegist");
         if (hasRegist == 1)
