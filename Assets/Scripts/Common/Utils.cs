5624800666491c3d4a42f3994c0babb0c21f52e5
zhuguoqing <1521292690@qq.com>
2024-02-05 19:50:20 +0800

    Update Utils.cs

--- Assets/Scripts/Common/Utils.cs
+++ Assets/Scripts/Common/Utils.cs
@@ -41,30 +41,30 @@ public class Utils : MonoBehaviour
         return true;
     }
 
-    // mainScene lock
-    public bool mapLock = false;
-    public bool contactLock = false;
-    public bool dwLock = false;
-    public bool promotionLock = false;
-    public bool shootingLock = false;
-
-    // map lock
-    public bool isStudioLock = false;
-    public bool isLibraryLock = false;
-    public bool isCafeLock = false;
-
-
     //// mainScene lock
-    //public bool mapLock = true;
-    //public bool contactLock = true;
-    //public bool dwLock = true;
-    //public bool promotionLock = true;
-    //public bool shootingLock = true;
+    //public bool mapLock = false;
+    //public bool contactLock = false;
+    //public bool dwLock = false;
+    //public bool promotionLock = false;
+    //public bool shootingLock = false;
 
     //// map lock
-    //public bool isStudioLock = true;
-    //public bool isLibraryLock = true;
-    //public bool isCafeLock = true;
+    //public bool isStudioLock = false;
+    //public bool isLibraryLock = false;
+    //public bool isCafeLock = false;
+
+
+    // mainScene lock
+    public bool mapLock = true;
+    public bool contactLock = true;
+    public bool dwLock = true;
+    public bool promotionLock = true;
+    public bool shootingLock = true;
+
+    // map lock
+    public bool isStudioLock = true;
+    public bool isLibraryLock = true;
+    public bool isCafeLock = true;
 
     // 记录拍摄轮次，第二轮结尾提示进入dw
     public int shootingIndex = 0;
