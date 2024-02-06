1dc3a23df0640c5e5392818937f89fbd5a0d5fb3
zhuguoqing <1521292690@qq.com>
2024-02-06 15:41:32 +0800

    update

--- Assets/Scripts/ProfileScene.cs
+++ Assets/Scripts/ProfileScene.cs
@@ -1,10 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ProfileScene : MonoBehaviour
 {
-    public void OnClickBack() {
+
+    public TMP_Text nameLal;
+    public TMP_Text ageLal;
+    public TMP_Text genderLal;
+
+    public TMP_Text romance;
+    public TMP_Text sci;
+    public TMP_Text action;
+    public TMP_Text comdy;
+    public TMP_Text mystery;
+
+    public TMP_Text facial;
+    public TMP_Text dialgue;
+    public TMP_Text sing;
+    public TMP_Text dancing;
+    public TMP_Text body;
+
+
+    public TMP_Text collaboration;
+    public TMP_Text likability;
+
+
+    public TMP_Text philsophy;
+    public TMP_Text fortune;
+    public TMP_Text aesthetic;
+
+
+    public void OnClickBack()
+    {
         Destroy(gameObject);
     }
 }
